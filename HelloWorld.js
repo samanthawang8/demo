d0573ef42627020ec41c4b8149757361cd510b2d
Xiaomeng Wang <Meng@xiaomengs-imac.lan>
2020-01-20 20:02:18 -0500

    add file

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-console.log("Hello World");
-=======
-console.log("Hello World Updated!!");
 console.log("Hello World!!");
->>>>>>> fa8d3c42dbe695b9e3c669443e4dd3427011b50e
+console.log("Hello World!!");
